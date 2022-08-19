7d645b24ee22fe1ca49b6f688482cd76b1dbdb67
YatechDev <106195439+YatechDev@users.noreply.github.com>
2022-08-19 12:25:04 +0100

    Add favicon, final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 
 })
@@ -21,6 +27,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25 + 1);
     let num2 = Math.floor(Math.random() * 25 + 1);
@@ -32,8 +42,8 @@ function runGame(gameType) {
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     } else {
-        alert(`Uknown game type: ${gameType}`);
-        throw `Uknown game type: ${gameType}. Aborting!`;
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
@@ -60,7 +70,7 @@ function checkAnswer() {
 }
 
 /**
- * gets the operands (the numbers) and the operator (plus, minus etc)
+ * gets the operands (the numbers) and the operator (plus, minus, etc)
  * directly from the dom, and returns the correct answer.
  */
 function calculateCorrectAnswer() {
